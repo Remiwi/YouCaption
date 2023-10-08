b199531688b95b8d5aaa1d2c7ed46b1069728065
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-10-07 22:51:29 -0400

    Made navbar logo navigate to homescreen

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import styles from "./Navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <h1>
-        <mark>You</mark>Caption
-      </h1>
+      <Link href="/" style={{ textDecoration: "none" }}>
+        <h1>
+          <mark>You</mark>Caption
+        </h1>
+      </Link>
       <div>
         <p>Hello, stranger!</p>
         <button>Sign in</button>
