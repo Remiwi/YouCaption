77d9881603361ef71a9eb7fc034ffd84521bdfb6
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-11-07 17:05:55 -0500

    Added settings button to navbar

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import styles from "./Navbar.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [signedIn, setSignedIn] = useState(true); // TODO: replace with auth check
+
   return (
     <div className={styles.navbar}>
       <Link href="/" style={{ textDecoration: "none" }}>
@@ -10,8 +16,22 @@ export default function Navbar() {
         </h1>
       </Link>
       <div>
-        <p>Hello, stranger!</p>
-        <button>Sign in</button>
+        <p>{signedIn ? "Hello, friend!" : "Hello, stranger!"}</p>
+        {signedIn && (
+          <Link href="/settings" style={{ textDecoration: "none" }}>
+            <button className={styles.settings}>
+              <Image
+                src="/icons/settings.png"
+                alt="settings image"
+                width={500}
+                height={500}
+              />
+            </button>
+          </Link>
+        )}
+        <button onClick={() => setSignedIn(!signedIn)}>
+          {signedIn ? "Sign out" : "Sign in"}
+        </button>
       </div>
     </div>
   );
