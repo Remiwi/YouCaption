a8ddb807703e5c369e7838fc6ce23d0d3ee25942
WontonSoup <zywinston@gmail.com>
2023-11-26 17:16:54 -0500

    fixed getusername call

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -12,15 +12,28 @@ import test from "node:test";
 export default function Navbar() {
   const usernameQuery = useQuery({
     queryKey: ["username"],
-    queryFn: () =>
-      fetch("http://localhost:8000/getUsername", {
-        credentials: "include",
-        mode: "cors",
-      }).then((res) => res.json()),
+    queryFn: async () => {
+        const response = await test_getUsername();
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    }
   });
+  
   const signedIn = usernameQuery.isSuccess && usernameQuery.data.signedIn;
   const username = signedIn ? usernameQuery.data.username : "...";
 
+  async function test_getUsername() {
+    let response = await fetch("http://127.0.0.1:8000/getUsername", {
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+    })
+    console.log(response)
+    return response
+  }
+  
   async function test_logout() {
     let response = await fetch("http://127.0.0.1:8000/logout", {
       method: "POST",
