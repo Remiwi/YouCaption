f1b661ef7f6baa9c16415c81377a57a465346cf8
WontonSoup <zywinston@gmail.com>
2023-11-26 17:16:01 -0500

    fixed None error

--- backend/app/routers/isLoggedIn.py
+++ backend/app/routers/isLoggedIn.py
@@ -8,17 +8,18 @@ from dependencies import get_db_conn, verify
 
 router = APIRouter()
 
-
 @router.get("/getUsername")
 async def getUsername(request: Request):
+    print("Getting Username")
     sessionid = request.cookies.get("sessionid")
+    print("User sessionID: ", sessionid)
     if sessionid:
-        query = "SELECT username FROM sessions JOIN users ON session.userGID = users.googleID WHERE sessionID = %s"
+        query = "SELECT username FROM sessions JOIN users ON sessions.userGID = users.googleID WHERE sessionID = %s"
         with closing(get_db_conn()) as conn:
             with closing(conn.cursor()) as cursor:
                 cursor.execute(query, (sessionid,))
                 username = cursor.fetchone()
-                if username[0]:
+                if username and username[0]:
                     return {"username": username[0], "signedIn": True}
                 else:
                     return {"username": "test", "signedIn": False}
