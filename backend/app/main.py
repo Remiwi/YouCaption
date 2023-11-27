5c9003676c284d6f75f6614894514df70cacc4c6
WontonSoup <zywinston@gmail.com>
2023-11-27 18:49:38 -0500

    added editor router

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from typing import Annotated, Optional
 from contextlib import closing
 from database import get_db_conn
 from fastapi.middleware.cors import CORSMiddleware
-from routers import login, isLoggedIn
+from routers import login, isLoggedIn, editor
 
 app = FastAPI()
 
@@ -22,6 +22,7 @@ app.add_middleware(
 
 app.include_router(login.router)
 app.include_router(isLoggedIn.router)
+app.include_router(editor.router)
 
 
 @app.middleware('http')
