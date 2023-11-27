4379df29f7da1a3177df7ae451f8bd58bff2e287
WontonSoup <zywinston@gmail.com>
2023-11-27 15:28:01 -0500

    added button for testing getting the following list

--- frontend/app/endpointTest/page.tsx
+++ frontend/app/endpointTest/page.tsx
@@ -31,11 +31,20 @@ export default function Test() {
             </button>
             <button
                 onClick={async () =>  console.log(await (await fetch("http://127.0.0.1:8000/follow/UniqueBaron8851", {
+                    method: "POST",
                     mode: "cors",
                     credentials: "include",
                 })).json())
                 }
                 > Follow
+            </button>
+            <button
+                onClick={async () =>  console.log(await (await fetch("http://127.0.0.1:8000/followingList", {
+                    mode: "cors",
+                    credentials: "include",
+                })).json())
+                }
+                > Get Following List
             </button>
         </>
     )
